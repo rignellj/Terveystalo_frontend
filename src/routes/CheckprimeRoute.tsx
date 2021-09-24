5e4e9805dbd3d06233f6104e7a8174e516e78632
Jere Rignell <jrignell@student.hive.fi>
2021-09-24 12:55:14 +0300

    conditional styles for text color

--- src/routes/CheckprimeRoute.tsx
+++ src/routes/CheckprimeRoute.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import Modal from '../components/UI/Modal/Modal';
 import { useTypedSelector } from '../hooks/useTypedSelector';
 
-import CheckPrime from '../pages/CheckPrime/CheckPrime';
 import { ActionTypeModal } from '../store/action-types';
+import CheckPrime from '../pages/CheckPrime/CheckPrime';
+import Modal from '../components/UI/Modal/Modal';
+
+import styles from './Prime.module.css';
 
 const CheckPrimeRoute: React.FC = () => {
 	const { modalIsOpen } = useTypedSelector(state => state.modal);
 	const { isPrime } = useTypedSelector(state => state.primeNumber);
 	let isPrimeText = 'Number is prime number';
-
+	let classes = styles.isPrime;
+	
 	if (!isPrime) {
 		isPrimeText = 'Number is not prime number';
+		classes = styles.notPrime;
 	}
 
 	return (
 		<React.Fragment>
 			<Modal closeButton closeModal={ActionTypeModal.IS_CLOSED} modalIsOpen={modalIsOpen} header='Result'>
-				<p>{isPrimeText}</p>
+				<p className={classes}>{isPrimeText}</p>
 			</Modal>
 			<CheckPrime />
 		</React.Fragment>
--- src/routes/SumRoute.tsx
+++ src/routes/SumRoute.tsx
@@ -5,19 +5,23 @@ import { useTypedSelector } from '../hooks/useTypedSelector';
 import Modal from '../components/UI/Modal/Modal';
 import Sum from '../pages/Sum/Sum';
 
+import styles from './Prime.module.css';
+
 const SumRoute: React.FC = () => {
 	const { modalIsOpen } = useTypedSelector(state => state.modal);
 	const { result, isPrime } = useTypedSelector(state => state.primeNumber);
 	let isPrimeText = 'is prime number';
+	let classes = styles.isPrime;
 
 	if (!isPrime) {
 		isPrimeText = 'is not prime number';
+		classes = styles.notPrime;
 	}
 
 	return (
 		<React.Fragment>
 			<Modal closeButton closeModal={ActionTypeModal.IS_CLOSED} modalIsOpen={modalIsOpen} header='Result'>
-				<p>Sum of numbers: {result}, number {isPrimeText}</p>
+				<p className={classes}>Sum of numbers: {result}, number {isPrimeText}</p>
 			</Modal>
 			<Sum />
 		</React.Fragment>
