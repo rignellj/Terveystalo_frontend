e17e29a4868b3922e873fdb4d2b5450c4067dc10
Jere Rignell <jrignell@student.hive.fi>
2021-09-25 11:41:01 +0300

    del dotenv

--- src/utils/config.ts
+++
@@ -1,6 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const SUM_ENDPOINT = process.env.REACT_APP_API_SUM;
-export const CHECK_PRIME_ENDPOINT = process.env.REACT_APP_API_CHECKPRIME;
